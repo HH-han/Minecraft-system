8c7150aef30f05d387ee1e8297e62973485c2f3b
bolan <2560177364@qq.com>
2026-05-03 09:40:13 +0800

    Minecraft-system

--- Minecraft/src/main/java/com/minecraft/mapper/UserMapper.java
+++ Minecraft/src/main/java/com/minecraft/mapper/UserMapper.java
@@ -3,13 +3,14 @@ package com.minecraft.mapper;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.minecraft.entity.User;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Update;
 
 @Mapper
 public interface UserMapper extends BaseMapper<User> {
     @Update("UPDATE user SET points = points + #{points} WHERE id = #{userId}")
-    int addPoints(Long userId, Integer points);
+    int addPoints(@Param("userId") Long userId, @Param("points") Integer points);
     
     @Update("UPDATE user SET points = points - #{points} WHERE id = #{userId} AND points >= #{points}")
-    int deductPoints(Long userId, Integer points);
+    int deductPoints(@Param("userId") Long userId, @Param("points") Integer points);
 }
