f186207a2f752ca38ed6fdf9c9eafc941994cc1d
bolan <2560177364@qq.com>
2026-07-09 23:21:09 +0800

    Minecraft-system

--- Minecraft/src/main/java/com/minecraft/config/CaptchaResourceConfiguration.java
+++ Minecraft/src/main/java/com/minecraft/config/CaptchaResourceConfiguration.java
@@ -6,19 +6,45 @@ import cloud.tianai.captcha.resource.impl.LocalMemoryResourceStore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.File;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Configuration
 public class CaptchaResourceConfiguration {
+
+    private static final String IMAGE_DIR = "D:/Image";
 
     @Bean
     public ResourceStore resourceStore() {
         LocalMemoryResourceStore resourceStore = new LocalMemoryResourceStore();
         
-        resourceStore.addResource(CaptchaTypeConstant.SLIDER, new Resource("classpath", "captcha/bg1.jpg", "default"));
-        resourceStore.addResource(CaptchaTypeConstant.SLIDER, new Resource("classpath", "captcha/bg2.jpg", "default"));
-        resourceStore.addResource(CaptchaTypeConstant.SLIDER, new Resource("classpath", "captcha/bg3.jpg", "default"));
-        resourceStore.addResource(CaptchaTypeConstant.SLIDER, new Resource("classpath", "captcha/bg4.jpg", "default"));
-        resourceStore.addResource(CaptchaTypeConstant.SLIDER, new Resource("classpath", "captcha/bg5.jpg", "default"));
+        List<String> imageFiles = getImageFiles(IMAGE_DIR);
+        
+        for (String imageFile : imageFiles) {
+            resourceStore.addResource(CaptchaTypeConstant.SLIDER, new Resource("file", imageFile, "default"));
+        }
         
         return resourceStore;
+    }
+    
+    private List<String> getImageFiles(String dirPath) {
+        File dir = new File(dirPath);
+        if (!dir.exists() || !dir.isDirectory()) {
+            return Arrays.asList();
+        }
+        
+        return Arrays.stream(dir.listFiles())
+                .filter(file -> file.isFile() && isImageFile(file.getName()))
+                .map(File::getAbsolutePath)
+                .limit(10)
+                .collect(Collectors.toList());
+    }
+    
+    private boolean isImageFile(String fileName) {
+        String lowerName = fileName.toLowerCase();
+        return lowerName.endsWith(".jpg") || lowerName.endsWith(".jpeg") || 
+               lowerName.endsWith(".png") || lowerName.endsWith(".webp");
     }
 }
